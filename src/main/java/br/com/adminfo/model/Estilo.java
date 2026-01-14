7948e38a0717db2d2000219f1a2da7e284ad7f89
Belini <fernando.beleboni@hotmail.com>
2026-01-13 21:52:37 -0300

    2026-01-13

---
+++ src/main/java/br/com/adminfo/model/Estilo.java
@@ -0,0 +1,67 @@
+package br.com.adminfo.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estilo")
+public class Estilo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	private String nome;
+	
+	@OneToMany(mappedBy = "estilo")
+	private List<Cerveja> cervejas;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cerveja> getCervejas() {
+		return cervejas;
+	}
+
+	public void setCervejas(List<Cerveja> cervejas) {
+		this.cervejas = cervejas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		return Objects.equals(codigo, other.codigo);
+	}	
+
+}
